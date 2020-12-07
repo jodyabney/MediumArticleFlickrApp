7ebb192fad247f1ee094e14a07ba91840a5b8fe2
Jody Abney <jodyabney@gmail.com>
2020-12-06 20:52:45 -0500

    Initial Commit

---
+++ MediumArticleFlickrApp/MediumArticleFlickrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumArticleFlickrAppApp.swift
+//  MediumArticleFlickrApp
+//
+//  Created by Jody Abney on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct MediumArticleFlickrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
